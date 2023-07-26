d7826de637cf9c9f1b81702b92e9b561ad4a47d9
Kkangjn <jjun1401@naver.com>
2023-07-26 20:05:47 +0900

    [Fix] 잠시보관

--- src/main/java/sparta/kingdombe/domain/story/service/StoryService.java
+++ src/main/java/sparta/kingdombe/domain/story/service/StoryService.java
@@ -1,6 +1,7 @@
 package sparta.kingdombe.domain.story.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
